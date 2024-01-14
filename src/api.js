2f2c8bc21fb958ed850296183bc3985172e4a569
Cynthia Phan <cynthiaphan.code@gmail.com>
2024-01-13 19:12:38 -0500

    upload files

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '39c6e2c34cmshf9886bb9320a4a1p1d4505jsnc14b94e84d83',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "9be883c6613b354090002d48589d6a1a";
